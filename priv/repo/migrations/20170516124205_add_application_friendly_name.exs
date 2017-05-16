4d28ac9c5804f3a0b7a1d8c3cec808b11efcb556
alisinabh <alisina.bm@gmail.com>
2017-05-16 18:23:13 +0430

    Add firendly name migration

--- priv/repo/migrations/20170516124205_add_application_friendly_name.exs
+++ priv/repo/migrations/20170516124205_add_application_friendly_name.exs
@@ -2,6 +2,8 @@ defmodule Catalyst.Repo.Migrations.AddApplicationFriendlyName do
   use Ecto.Migration
 
   def change do
-
+    alter table(:applications) do
+      add :friendly_name, :string, size: 100
+    end
   end
 end
